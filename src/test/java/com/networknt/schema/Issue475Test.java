24c9aef76b3e43b601b3a5bfad1af2e62e05bc34
Steve Hu <stevehu@gmail.com>
2021-10-26 18:28:53 -0400

    fixes #475 json schema cannot be validated by the meta schema

---
+++ src/test/java/com/networknt/schema/Issue475Test.java
@@ -0,0 +1,34 @@
+package com.networknt.schema;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.util.Set;
+
+public class Issue475Test {
+    protected JsonSchema getJsonSchemaFromStreamContentV291909(InputStream schemaContent) {
+        JsonSchemaFactory factory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V201909);
+        return factory.getSchema(schemaContent);
+    }
+
+    protected JsonNode getJsonNodeFromStreamContent(InputStream content) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readTree(content);
+    }
+
+    @Test
+    public void shouldWork() throws Exception {
+        String schemaPath = "/schema/issue475-2019-09.json";
+        String dataPath = "/data/issue475.json";
+        InputStream schemaInputStream = getClass().getResourceAsStream(schemaPath);
+        JsonSchema schema = getJsonSchemaFromStreamContentV291909(schemaInputStream);
+        InputStream dataInputStream = getClass().getResourceAsStream(dataPath);
+        JsonNode node = getJsonNodeFromStreamContent(dataInputStream);
+        Set<ValidationMessage> errors = schema.validate(node);
+        Assertions.assertEquals(0, errors.size());
+    }
+
+}
