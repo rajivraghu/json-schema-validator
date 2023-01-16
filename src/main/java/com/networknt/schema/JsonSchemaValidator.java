8f70c3bcc7f88f63184a2c24a2a1db5e49c40626
rajivraghu <rajiv.vedavyas@gmail.com>
2023-01-16 13:17:09 +0000

    customattribute

--- src/main/java/com/networknt/schema/JsonSchemaValidator.java
+++ src/main/java/com/networknt/schema/JsonSchemaValidator.java
@@ -1,4 +1,5 @@
 package com.networknt.schema;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.github.fge.jackson.JsonLoader;
 import com.networknt.schema.JsonSchema;
@@ -8,7 +9,11 @@ import com.networknt.schema.SpecVersion.VersionFlag;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class JsonSchemaValidator {
 
@@ -16,6 +21,8 @@ public class JsonSchemaValidator {
         // Load the JSON Schema file
         File schemaFile = new File("/workspaces/json-schema-validator/src/main/resources/schema.json");
         JsonNode schemaNode = JsonLoader.fromFile(schemaFile);
+
+     
 
         // Create a JSON Schema object from the schema file
         
@@ -32,7 +39,34 @@ public class JsonSchemaValidator {
 
         // Print any validation errors
         for (ValidationMessage error : errors) {
-            System.out.println(error.getMessage());
+            if(error.getType().equalsIgnoreCase("required")){
+                Map<String, String> requiredMap=  new HashMap<>();
+                JsonNode requiredNode = schemaNode.get("custom");
+                if(requiredNode.isObject()){                
+                    if(requiredNode.isObject()){
+                    requiredNode.fields().forEachRemaining(entry->{
+                    requiredMap.put(entry.getKey(), entry.getValue().asText());
+                     });
+                    }
+
+                }
+             
+                String input = error.getMessage()   ;
+                String fieldName = null;
+                Pattern pattern = Pattern.compile("\\$.\\w+");
+                Matcher matcher = pattern.matcher(input);
+                if (matcher.find()) {
+                fieldName = matcher.group();
+             }
+
+        System.out.println("fieldName-->"+fieldName); // prints "$.companyName"
+        System.out.println( requiredMap.get(fieldName));
+
+            }
+            else {
+                System.out.println(error.getMessage());
+
+            }
         }
     }
 }
